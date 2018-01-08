37a318c99be1da91068e53728bcd580477178843
chenqf <feng_er@outlook.com>
2018-01-08 18:59:42 +0800

    http://jira.deviceone.net:8088/browse/PRODDEV-5902

--- doExt_do_Network/src/doext/implement/do_Network_Model.java
+++ doExt_do_Network/src/doext/implement/do_Network_Model.java
@@ -206,11 +206,44 @@ public class do_Network_Model extends DoSingletonModule implements do_Network_IM
 	}
 
 	private String getLocalMacAddress() {
-		WifiManager wifiMgr = (WifiManager) DoServiceContainer.getPageViewFactory().getAppContext().getSystemService(Context.WIFI_SERVICE);
-		WifiInfo info = (null == wifiMgr ? null : wifiMgr.getConnectionInfo());
-		String macAddress = "";
-		if (null != info) {
-			macAddress = info.getMacAddress();
+		/*
+		 * 获取mac地址有一点需要注意的就是android
+		 * 6.0版本后，以下注释方法不再适用，不管任何手机都会返回"02:00:00:00:00:00"
+		 * 这个默认的mac地址，这是googel官方为了加强权限管理而禁用了getSYstemService
+		 * (Context.WIFI_SERVICE)方法来获得mac地址。
+		 */
+		// String macAddress= "";
+		// WifiManager wifiManager = (WifiManager)
+		// MyApp.getContext().getSystemService(Context.WIFI_SERVICE);
+		// WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		// macAddress = wifiInfo.getMacAddress();
+		// return macAddress;
+
+		String macAddress = "02:00:00:00:00:00";
+		StringBuffer buf = new StringBuffer();
+		NetworkInterface networkInterface = null;
+		try {
+			networkInterface = NetworkInterface.getByName("wlan0");
+			if (networkInterface == null) {
+				networkInterface = NetworkInterface.getByName("eth1");
+			}
+			if (networkInterface == null) {
+				return "02:00:00:00:00:00";
+			}
+			byte[] addr = networkInterface.getHardwareAddress();
+			if (addr == null || addr.length < 1) {
+				return "02:00:00:00:00:00";
+			}
+			for (byte b : addr) {
+				buf.append(String.format("%02x:", b));
+			}
+			if (buf.length() > 0) {
+				buf.deleteCharAt(buf.length() - 1);
+			}
+			macAddress = buf.toString();
+		} catch (SocketException e) {
+			e.printStackTrace();
+			return "02:00:00:00:00:00";
 		}
 		return macAddress;
 	}
@@ -224,7 +257,7 @@ public class do_Network_Model extends DoSingletonModule implements do_Network_IM
 		WifiInfo info = wifiManager.getConnectionInfo();
 		String _currentWifiName = info != null ? info.getSSID() : null;
 		JSONObject _obj0 = new JSONObject();
-		_obj0.put("currentWifiName", _currentWifiName.replaceAll("\"",""));
+		_obj0.put("currentWifiName", _currentWifiName.replaceAll("\"", ""));
 		JSONArray _array = new JSONArray();
 		_obj0.putOpt("wifiNameList", _array);
 		_obj0.putOpt("routerMacAddress", info.getBSSID());
